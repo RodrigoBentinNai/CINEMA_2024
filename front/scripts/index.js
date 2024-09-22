16ad8eeb5ff298e1aedffcdcfc42b6a98fd5be0c
rodrigo mario bentin nai <rodrigo.bentin.rbn@gmail.com>
2024-09-21 21:18:01 -0300

    07

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,13 +1,41 @@
+const axios = require("axios");
 const container = document.getElementById("movies-container");
 const formatCards = require("./formatCards");
 
-$.get("https://students-api.up.railway.app/movies")
-  .done( (informacion) => {
-    const moviesCards = informacion.map(formatCards);
+// ! AJAX ↓↓↓
+// $.get("https://students-api.up.railway.app/movies")
+//   .done( (informacion) => {
+//     const moviesCards = informacion.map(formatCards);
+//     moviesCards.forEach((movieHtml) => container.appendChild(movieHtml));
+//   })
+//   .fail( () => {
+//     alert("La informacion del servidor NO pudo cargarse, se utilizara FAKE info");
+//     const moviesCards = tempData.map(formatCards);
+//     moviesCards.forEach((movieHtml) => container.appendChild(movieHtml));
+//   });
+
+// ! AXIOS ↓↓↓
+axios.get("https://students-api.up.railway.app/movies")
+  .then( (informacion) => {
+    const moviesCards = informacion.data.map(formatCards);
     moviesCards.forEach((movieHtml) => container.appendChild(movieHtml));
   })
-  .fail( () => {
+  .catch( () => {
     alert("La informacion del servidor NO pudo cargarse, se utilizara FAKE info");
     const moviesCards = tempData.map(formatCards);
     moviesCards.forEach((movieHtml) => container.appendChild(movieHtml));
   });
+
+// ! ASYNC/AWAIT ↓↓↓
+// const getAllMovies = async () => {
+//   try {
+//     const response = await axios.get("https://students-api.up.railway.app/movies");
+//   return response.data;
+//   } catch (error) {
+//     alert("La informacion del servidor NO pudo cargarse, se utilizara FAKE info");
+//     const moviesCards = tempData.map(formatCards);
+//     moviesCards.forEach((movieHtml) => container.appendChild(movieHtml));
+//   }
+// };
+
+// getAllMovies();
